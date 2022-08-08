e3f70245047c45bebe2dcb67df8158a90529ce04
Matthew Scott <m.valckescott@gmail.com>
2022-08-07 21:00:20 -0700

    change lr for num stability

--- reusefiles/trainvaefrommnist.jl
+++ reusefiles/trainvaefrommnist.jl
@@ -7,12 +7,12 @@ include("./trainloops.jl")
 
 function trainvaefromMNIST()
     vaemodel = makevae()
-    loss = vaeloss(vaemodel, 1f0, 0.01f0)
-    traindata = reshape(MNIST(Float32,:train).features[:,:,1:end] |> gpu, 28^2, :)
-    testdata = reshape(MNIST(Float32,:test).features[:,:,1:end] |> gpu, 28^2, :)
+    loss = vaeloss(vaemodel, 1.0f0, 0.01f0)
+    traindata = reshape(MNIST(Float32, :train).features[:, :, 1:end] |> gpu, 28^2, :)
+    testdata = reshape(MNIST(Float32, :test).features[:, :, 1:end] |> gpu, 28^2, :)
     trainloader = DataLoader(traindata, batchsize=32, shuffle=true)
     testloader = DataLoader(testdata, batchsize=32, shuffle=true)
-    trainvalidatelognsave(loss, vaemodel, params(vaemodel), trainloader, testloader, Flux.Optimise.ADAM(0.001), 100, "./reusefiles/models/","./reusefiles/logs/",saveinterval = 10, validateinterval=5)
+    trainvalidatelognsave(loss, vaemodel, params(vaemodel), trainloader, testloader, Flux.Optimise.ADAM(0.0001), 100, "./reusefiles/models/", "./reusefiles/logs/", saveinterval=10, validateinterval=5)
 end
 
 trainvaefromMNIST()
